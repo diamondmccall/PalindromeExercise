f407dcd893306fa157377d45142df1ce5e17a5d4
Diamond Mccall <singlikediamond@gmail.com>
2023-02-15 21:01:54 -0600

    inital commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace PalindromeExercise
+{
+	public class WordSmith
+	{
+		public bool IsPalindrome(string word)
+		{
+			var reversed = "";
+			for (int i = word.Length - 1; i >= 0; i--) 
+			{
+				reversed += word[i];
+			}
+			if(word == reversed)
+			{
+				return true;
+			}
+			return false;
+		}
+	}
+}
+
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,22 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hi", false)]
 
+        public void MyTesting(string word, bool expected)
+        {
+            var palindrome = new WordSmith();
+
+            var actual = palindrome.IsPalindrome(word);
+
+            Assert.Equal(expected,actual);
         }
     }
 }
